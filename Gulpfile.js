8f12f3c82a98a0a872dd87269ff316116779244e
Goemans.Stephan <quizdroid@gmail.com>
2014-11-25 17:01:09 +0100

    Added 'usemin' support

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,11 +4,15 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
 var jasmine = require('gulp-jasmine');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var jasminePhantomJs = require('gulp-jasmine2-phantomjs');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-usemin');
+var rev = require('gulp-rev');
 
 gulp.task("lint", function() {
 	return gulp.src("src/**/*.js")
@@ -18,5 +22,15 @@ gulp.task("lint", function() {
 gulp.task("test", function() {
 	return gulp.src("test/**/*.html").pipe(jasminePhantomJs());
 });
+
+gulp.task('build', function() {
+	gulp.src('src/*.html')
+		.pipe(usemin({
+			css: [minifyCss(), 'concat'],
+			html: [minifyHtml({empty: true})],
+			js: [uglify(), rev()]
+		}))
+		.pipe(gulp.dest('dist/'));
+});
 // Default Task
-gulp.task('default', ['lint', /* 'sass', */ 'test' /*, 'watch' */]);
+gulp.task('default', ['lint',  'test' ]);
